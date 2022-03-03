58f69e81ec432bcf5c8d0fea146d01cb86629cf8
chuuuya12 <96934806+myoblivion@users.noreply.github.com>
2022-03-04 01:00:56 +0800

    commit

--- src/components/gogoworld.jsx
+++ src/components/gogoworld.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FaRegArrowAltCircleUp } from "react-icons/fa";
 import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
 import f1 from "../images/collection1.png";
 import f2 from "../images/collection2.jpg";
 import f3 from "../images/collection3.png";
@@ -10,7 +11,21 @@ import f6 from "../images/collection6.png";
 import f7 from "../images/collection7.png";
 import image from "../images/banner1.png";
 import image2 from "../images/gogoworlds.jpg";
-const GoGoWorldComponent = () => {
+import icono from "../images/icons/logo.png";
+import {
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaMapMarked,
+  FaPhone,
+  FaReddit,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+
+
+const GoGoWorldComponent = ({ currentRoute }) => {
   // Title
   useEffect(() => {
     document.title = "Black Spot Studio | GoGo World";
@@ -19,8 +34,8 @@ const GoGoWorldComponent = () => {
     <div className="gogoworld wrapper">
       <section className="gogoworld-wrapper">
         <h1>Coming Soon</h1>
-        <img src={image} alt="Gogo World" id="first-image"/>
-        <img src={image2} alt="Gogo World Mobile image" id="second-image"/>
+        <img src={image} alt="Gogo World" id="first-image" />
+        <img src={image2} alt="Gogo World Mobile image" id="second-image" />
       </section>
 
       <section className="gogoworld-features">
@@ -50,11 +65,109 @@ const GoGoWorldComponent = () => {
           </ul>
         </div>
       </section>
+      <div className="section">
+        <footer className="footer-distributed">
+          <div className="footer-left">
+            <img src={icono} alt="" className="logso" />
+            <p className="footer-links">
+              <Link to="/about">About Us</Link>
+              <Link to="/privacy">Privacy & Policy</Link>
+              <Link to="/job">Career</Link>
 
-      <div className="bootons">
-        <HashLink smooth to="/gogoworld#" id="up">
-          <FaRegArrowAltCircleUp />
-        </HashLink>
+              <HashLink smooth to="/#contact">
+                {" "}
+                Contact Us
+              </HashLink>
+            </p>
+
+            <p className="footer-company-name">
+              Copyright © Black Spot Studio PH{" "}
+            </p>
+          </div>
+
+          <div className="footer-center">
+            <div className="desz">
+              <p>
+                <Link
+                  to="/direction"
+                  style={
+                    currentRoute === "direction"
+                      ? { borderBottom: "2px solid yellow" }
+                      : { borderBottom: "2px solid transparent" }
+                  }
+                >
+                  <FaMapMarked /> 26th St, Taguig, 1634 Metro Manila
+                </Link>
+              </p>
+            </div>
+
+            <div className="desz">
+              <p>
+                <a href="mailto:support@company.com">
+                  {" "}
+                  <FaEnvelope /> support@company.com
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div className="footer-right">
+            <div className="footer-icons">
+              <ul className="social-media-list">
+                <li>
+                  <a
+                    href="https://www.facebook.com/BlackspotstudioPH"
+                    target="_blank"
+                    className="contact-icon"
+                  >
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/phblackspotstudio/"
+                    target="_blank"
+                    className="contact-icon"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/BlackSpotPH"
+                    target="_blank"
+                    className="contact-icon"
+                  >
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.reddit.com/r/BlackSpotStudioPH/"
+                    target="_blank"
+                    className="contact-icon"
+                  >
+                    <FaReddit />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UCL9FkYZu775aKfJ1zv4gZJQ"
+                    target="_blank"
+                    className="contact-icon"
+                  >
+                    <FaYoutube />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+        <div className="bootons">
+          <HashLink smooth to="/gogoworld#" id="up">
+            <FaRegArrowAltCircleUp />
+          </HashLink>
+        </div>
       </div>
     </div>
   );
