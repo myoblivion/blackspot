284be7233d4f2a338135c600a04b8f822d606d73
France lee <96934806+myoblivion@users.noreply.github.com>
2022-04-26 17:03:59 +0800

    commit

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef, LinkProps } from "react";
 import { Admin, Resource } from "react-admin";
 
-const Admins = ({ props, ref, currentRoute }) => {
+const Admins = ({ record }) => {
   // Title
   useEffect(() => {
     document.title = "Black Spot Studio | Admin";
@@ -11,17 +11,23 @@ const Admins = ({ props, ref, currentRoute }) => {
       <div className="admin-wrapper">
         <div className="admin">
           <h1>Admin Panel</h1>
-
           <form autoComplete="off" id="backendstuff">
-
-            <label htmlFor="">Gogo Racing</label>
-            <input type="radio" name="" id="" />
-            <label htmlFor="">Scion Fist</label>
-            <input type="radio" />
-            <label htmlFor="">Gogo Chain</label>
-            <input type="radio" />
-            <label htmlFor="">Gogo World</label>
-            <input type="radio" />
+            <div className="admin_contents">
+              <div className="components">
+                <label htmlFor="choose1">Gogo Racing</label>
+                <input type="radio" name="choose" id="choose1" />
+                <label htmlFor="choose2">Scion Fist</label>
+                <input type="radio" name="choose" id="choose2" />
+                <label htmlFor="choose3">Gogo Chain</label>
+                <input type="radio" name="choose" id="choose3" />
+                <label htmlFor="choose4">Gogo World</label>
+                <input type="radio" name="choose" id="choose4" />
+              </div>
+              <div className="textfields">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" />
+              </div>
+            </div>
           </form>
         </div>
       </div>
