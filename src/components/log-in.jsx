5df6a5ed18719e93fe891929c9678b516d029b39
France lee <96934806+myoblivion@users.noreply.github.com>
2022-04-29 09:44:25 +0800

    commit

--- src/components/log-in.jsx
+++ src/components/log-in.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef, LinkProps } from "react";
 import { Link } from "react-router-dom";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { FaUserCircle, FaSearch, FaWindowClose } from "react-icons/fa";
+import firebase from "firebase";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -19,12 +20,11 @@ const firebaseConfig = {
   appId: "1:366890563540:web:00b2063a519c11420d0841",
   measurementId: "G-1Z34EP137Q",
 };
-
+const fire = firebase.initializeApp(firebaseConfig);
 const LogInComponent = ({ props, ref, currentRoute }) => {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-
 
   const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
@@ -32,9 +32,54 @@ const LogInComponent = ({ props, ref, currentRoute }) => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [hasAccount, setHasAccount] = useState("false");
-
-
-  const handleLogin = () => {};
+  const handleLogin = () => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+  const handleSignup = () => {
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+  const handleLogout = () => {
+    fire.auth().signout();
+  };
+  const authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      if(user){
+        setUser(user);
+      } else {
+        setUser("")
+      }
+    })
+  }
+  useEffect(() => {
+    
+  }, [])
   // Title
   useEffect(() => {
     document.title = "Black Spot Studio | Log in";
