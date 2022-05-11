95a45ea80f2f722cf1e89fb26a6c2a9e5e86413b
France lee <96934806+myoblivion@users.noreply.github.com>
2022-05-11 14:10:38 +0800

    commit

--- src/components/play-to-win-raffle.jsx
+++ src/components/play-to-win-raffle.jsx
@@ -20,7 +20,8 @@ const EarnToWinRaffle = ({ props, ref, currentRoute }) => {
     };
     axios
       .post(
-        "https://sheet.best/api/sheets/ca2b339e-10fe-4000-9f5f-b7e5449fab76",
+        "https://sheet.best/api/sheets/c24db70e-a771-4036-ab7a-afc359687c86",
+        "http://54.179.151.77",
         data
       )
       .then((response) => {
@@ -38,14 +39,14 @@ const EarnToWinRaffle = ({ props, ref, currentRoute }) => {
   };
   const getData = () => {
     axios
-      .get("https://sheet.best/api/sheets/9eda958b-9a8f-44e2-a8bd-3a08338c31cc")
+      .get("https://sheet.best/api/sheets/c24db70e-a771-4036-ab7a-afc359687c86")
       .then((response) => {
         setData(response.data);
       });
   };
   const Fetch = () => {
     fetch(
-      "https://sheet.best/api/sheets/9eda958b-9a8f-44e2-a8bd-3a08338c31cc",
+      "https://sheet.best/api/sheets/c24db70e-a771-4036-ab7a-afc359687c86",
       {
         headers: {
           "X-Api-Key":
