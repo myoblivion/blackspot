167adbe699d696db3f06ef831a63799df67fd5b4
France lee <francelee594@gmail.com>
2022-06-28 09:38:23 +0800

    commit

--- serverEditor/dataserver.js
+++ serverEditor/dataserver.js
@@ -1,7 +1,7 @@
 // server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("/Users/Admin/Desktop/blackspot/src/db.json");
+const router = jsonServer.router("/Users/Admin/Desktop/blackspot/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
