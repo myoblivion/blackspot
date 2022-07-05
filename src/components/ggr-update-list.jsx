2c73e03cf349097606f75630f0fb8ca3f79e9e45
France lee <francelee594@gmail.com>
2022-07-05 16:16:53 +0800

    commit

--- src/components/ggr-update-list.jsx
+++ src/components/ggr-update-list.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef, LinkProps } from "react";
-import { Routes, Route } from "react-router-dom";
+import image from "../images/banner4.png";
+import image2 from "../images/gogochain-mobile.png";
+import { HashLink } from "react-router-hash-link";
+import icono from "../images/icons/logo.png";
 import { Link } from "react-router-dom";
 import { FaUserCircle, FaSearch, FaWindowClose } from "react-icons/fa";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
-import PostsLinks from "./postLinks";
-import Post from "./Post";
 
-const GogoRacingUpdateList = ({ props, ref, currentRoute, posts }) => {
+import { AiFillInstagram } from "react-icons/ai";
+
+const GogoRacingUpdateList = ({ props, ref, currentRoute }) => {
   // Title
   useEffect(() => {
     document.title = "Black Spot Studio | Gogo Racing Update List";
@@ -22,7 +25,7 @@ const GogoRacingUpdateList = ({ props, ref, currentRoute, posts }) => {
           <div className="ggr-top">
             <div className="ggr-left">
               <Link
-                to="/ggr-update-list"
+                to="/gogoracingnews"
                 {...props}
                 ref={ref}
                 onClick={() => window.scrollTo(0, 0)}
@@ -45,10 +48,64 @@ const GogoRacingUpdateList = ({ props, ref, currentRoute, posts }) => {
             </div>
           </div>
           <div className="ggrlists-contents">
-            <Routes>
-              <Route path="/*" element={<PostsLinks posts={posts} />} />
-              <Route path="/:postId/*" element={<Post posts={posts} />} />
-            </Routes>
+            <ol>
+              <Link
+                to="/ggr-patch4"
+                {...props}
+                ref={ref}
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                <li>
+                  <div className="li-left"></div>
+                  <div className="li-right">
+                    <h3>🔊 1.0.4 Patch Note</h3>
+                    <span>06.13.2022</span>
+                  </div>
+                </li>
+              </Link>
+              <Link
+                to="/ggr-patch3"
+                {...props}
+                ref={ref}
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                <li>
+                  <div className="li-left"></div>
+                  <div className="li-right">
+                    <h3>🔊 1.0.3 Patch Note</h3>
+                    <span>04.04.2022</span>
+                  </div>
+                </li>
+              </Link>
+              <Link
+                to="/ggr-patch2"
+                {...props}
+                ref={ref}
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                <li>
+                  <div className="li-left"></div>
+                  <div className="li-right">
+                    <h3>🔊 1.0.2 Patch Note</h3>
+                    <span>03.25.2022</span>
+                  </div>
+                </li>
+              </Link>
+              <Link
+                to="/ggr-updates"
+                {...props}
+                ref={ref}
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                <li>
+                  <div className="li-left"></div>
+                  <div className="li-right">
+                    <h3>🔊 1.0.1 Patch Note</h3>
+                    <span>03.23.2022</span>
+                  </div>
+                </li>
+              </Link>
+            </ol>
           </div>
         </div>
       </div>
