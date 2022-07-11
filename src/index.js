0006bea0dc574a7fbb29d1d9a486972bfd74e3c9
France lee <francelee594@gmail.com>
2022-07-11 13:34:44 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./components/App";
 import { createBrowserHistory } from "history";
 const hist = createBrowserHistory();
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "http//localhost:8000/";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("app")
 );
