59fd2ef7be6a8914e4f79cc946781f21bf1288be
France lee <francelee594@gmail.com>
2022-07-26 14:39:18 +0800

    commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import Interweave from "interweave";
 import { deletePost } from "./actions/postActions";
 import backgroundimg from "../images/gogoracingbackground/ggrpatchnotesbanner.png";
 
 function Post({ posts }) {
-  const setPost = useState(null);
   const { postId } = useParams();
   const navigate = useNavigate();
   const post = posts && posts.find(({ id }) => `${id}` === postId);
-  useEffect(() => {
-    fetch("http://localhost:8000/posts")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setPost(data);
-      });
-  }, []);
   return (
     <React.Fragment>
       {post ? (
