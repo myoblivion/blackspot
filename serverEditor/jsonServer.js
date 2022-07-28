7885532576ecc05a8c5699d5c8275e543988f5b8
France lee <francelee594@gmail.com>
2022-07-28 15:17:55 +0800

    commit

--- serverEditor/jsonServer.js
+++ serverEditor/jsonServer.js
@@ -58,7 +58,42 @@ const db_connection = require("./database").promise();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+const allowedOrigins = [
+  "http://localhost:8081",
+  "http://blackspotstudio.ph",
+  "http://127.0.0:8081",
+  "*",
+];
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+console.log(allowedOrigins);
 app.post("/posts/new", async (req, res) => {
   try {
     const [rows] = await db_connection.execute(
@@ -72,7 +107,20 @@ app.post("/posts/new", async (req, res) => {
     console.log(err);
   }
 });
-
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "*",
+    "http://127.0.0.1:8081",
+    "http://blackspotstudio.ph/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "mutipart/form-data"
+  );
+  next();
+});
 app.get("/posts", async (req, res) => {
   try {
     const [rows] = await db_connection.execute("SELECT * FROM posts ");
